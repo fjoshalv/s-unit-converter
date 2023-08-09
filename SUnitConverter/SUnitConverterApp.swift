0a5bfaff9f2dba0b509674614523b0bc2c753619
Joshua Alvarado <fjoshalv@gmail.com>
2023-08-08 21:30:08 -0600

    Initial Commit

---
+++ SUnitConverter/SUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUnitConverterApp.swift
+//  SUnitConverter
+//
+//  Created by Fernando Alvarado on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
